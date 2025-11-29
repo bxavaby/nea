5c038ab9031f300cc8f4798f1f0721dbe06f3e10
bxavaby <bxavaby@protonmail.ch>
2025-11-30 00:10:41 +0100

    feat: block most scrapers

---
+++ js/sw.js
@@ -0,0 +1,12 @@
+/* ./js/sw.js */
+
+self.addEventListener("fetch", (event) => {
+  const ua = event.request.headers.get("user-agent") || "";
+
+  if (/bot|crawl|gptbot|anthropic|o1/i.test(ua)) {
+    event.respondWith(new Response("🤖 No", { status: 403 }));
+    return;
+  }
+
+  event.respondWith(fetch(event.request));
+});
